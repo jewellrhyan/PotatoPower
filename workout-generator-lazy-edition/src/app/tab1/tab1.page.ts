93567d6d2bb41af1df598820a5ce025035f8a25b
Brian Christopher Vinluan <bvinluan@uci.edu>
2023-03-19 20:17:08 -0700

    Added Google Login for Tab1

--- workout-generator-lazy-edition/src/app/tab1/tab1.page.ts
+++ workout-generator-lazy-edition/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
+import { isPlatform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  user: any = null;
+  apple = null;
 
-  constructor() {}
+  constructor() {
+    if (!isPlatform('capacitor')) {
+      GoogleAuth.initialize();
+    }
+  }
+
+  async signIn() {
+    this.user = await GoogleAuth.signIn();
+    console.log('user: ', this.user);
+  }
+
+  async refresh() {
+    const authCode = await GoogleAuth.refresh();
+    console.log('refresh: ', authCode);
+    // { accessToken: 'xxx', idToken: 'xxx' }
+  }
+
+  async signOut() {
+    await GoogleAuth.signOut();
+    this.user = null;
+  }
 
 }
